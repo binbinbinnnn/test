e2ee4ae7eb887a2ce1d87174fcdf74e4bab18bc0
binbinbinnnn <lukeqls2373@gmail.com>
2023-10-14 11:56:11 +0900

    백준 11382

---
+++ baekjoon/11382.py
@@ -0,0 +1,7 @@
+from sys import stdin
+stdin = open("baekjoon/input.txt", "r") # 테스트 파일을 읽을 때 사용합니다.
+
+line = stdin.readline() # 한 줄을 읽습니다.
+[nbr1, nbr2, nbr3] = map(int, line.split(' '))
+
+print(nbr1 + nbr2 + nbr3)
